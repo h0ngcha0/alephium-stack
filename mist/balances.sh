3e34e3ba1194510399899970323479b2bb2c4b4d
Hongchao Liu <lbs.lhc@gmail.com>
2021-11-08 22:08:11 +0100

    calculate total amount

--- mist/balances.sh
+++ mist/balances.sh
@@ -6,11 +6,17 @@ ADDRESSES="
     1EVr6MCWgucwQqQukA4HZimondaRjwouDPmcqsB2g5Jt6
     "
 
+SUM=0
+
 echo Balances:
 for ADDR in $ADDRESSES
 do
     BALANCE=$(curl -s https://mainnet-backend.alephium.org/addresses/$ADDR | jq ".balance" | tr -d '"')
     FORMATTED_BALANCE=$(echo "scale=5; $BALANCE / 1000000000000000000" | bc)
     echo $ADDR: $FORMATTED_BALANCE
+
+    SUM=$(echo "$SUM + $FORMATTED_BALANCE" | bc)
 done
 
+echo "---"
+echo "Total: "$SUM
