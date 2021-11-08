23987e4bbc80c14af7095285fb219ffe2cf514e0
Hongchao Liu <lbs.lhc@gmail.com>
2021-11-08 20:55:04 +0100

    add balances.sh

---
+++ mist/balances.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# List of your addresses
+ADDRESSES="
+    1EEiGRU3P88CeMhEYGsQBmzxknw3gtpncTE76jx7Y58mE
+    1EVr6MCWgucwQqQukA4HZimondaRjwouDPmcqsB2g5Jt6
+    "
+
+echo Balances:
+for ADDR in $ADDRESSES
+do
+    BALANCE=$(curl -s https://mainnet-backend.alephium.org/addresses/$ADDR | jq ".balance" | tr -d '"')
+    FORMATTED_BALANCE=$(echo "scale=5; $BALANCE / 1000000000000000000" | bc)
+    echo $ADDR: $FORMATTED_BALANCE
+done
+
